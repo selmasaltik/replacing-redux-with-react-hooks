d45c8c151f2b37361601bb9a94c5fd4d3b6fa982
selmasaltik <selmasaltiik@gmail.com>
2024-12-06 00:34:50 +0300

    Alternative: Using the Context API

--- 32-react-hooks-update/src/index.js
+++ 32-react-hooks-update/src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+import ProductsProvider from './context/products-context';
 
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
 
-const store = createStore(rootReducer);
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter 
       future={{
         v7_startTransition: true,
@@ -26,5 +20,5 @@ root.render(
     >
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
