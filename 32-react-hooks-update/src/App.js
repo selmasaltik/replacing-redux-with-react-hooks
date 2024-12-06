bd68e71b8a2e0fbb560892c42544c1a59fa7b974
selmasaltik <selmasaltiik@gmail.com>
2024-12-06 04:20:25 +0300

    Managing Multiple State Slices with the Custom Store Hook

--- 32-react-hooks-update/src/App.js
+++ 32-react-hooks-update/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Nav/Navigation';
 import ProductsPage from './containers/Products';
 import FavoritesPage from './containers/Favorites';
+import Counter from './containers/Counter';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<ProductsPage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
         </Routes>
+        <Counter />
       </main>
     </React.Fragment>
   );
